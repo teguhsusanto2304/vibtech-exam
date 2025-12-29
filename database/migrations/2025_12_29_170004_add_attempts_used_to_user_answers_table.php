ec9eb3267bb9a91e372d5d7174ac9c78ee6b26c0
Teguh Susanto <teguh.susanto@hotmail.com>
2025-12-29 16:01:38 +0700

    fix

---
+++ database/migrations/2025_12_29_170004_add_attempts_used_to_user_answers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_answers', function (Blueprint $table) {
+            //
+            $table->integer('attempts_used')->default(0)->after('user_exam_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_answers', function (Blueprint $table) {
+            //
+            $table->dropColumn('attempts_used');
+        });
+    }
+};
